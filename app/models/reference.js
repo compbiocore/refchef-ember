2c54bd9516f7c1cf9bd993757fc85f6b57f9d7ad
fernandogelin <fernandogelin@gmail.com>
2019-11-04 09:58:46 -0500

    feat: add attrs to reference model

--- app/models/reference.js
+++ app/models/reference.js
@@ -3,4 +3,14 @@ const { Model, attr, belongsTo } = DS;
 
 export default class ReferenceModel extends Model {
   @belongsTo() content;
+  @attr() levels;
+  @attr() name;
+  @attr() organism;
+  @attr() common_name;
+  @attr() description;
+  @attr() downloader;
+  @attr() organization;
+  @attr() taxon_id;
+  @attr() ensembl_release;
+  @attr() custom;
 }
--- app/serializers/reference.js
+++ app/serializers/reference.js
@@ -4,7 +4,15 @@ import ApplicationSerializer from './application';
 export default class ReferenceSerializer extends ApplicationSerializer {
   normalize(type, hash) {
     hash.id = hash.metadata.name;
-    console.log(hash);
+    hash.name = hash.metadata.name;
+    hash.organism = hash.metadata.organism;
+    hash.common_name = hash.metadata.common_name;
+    hash.description = hash.metadata.description;
+    hash.downloader = hash.metadata.downloader;
+    hash.organization = hash.metadata.organization;
+    hash.taxon_id = hash.metadata.ncbi_taxon_id;
+    hash.ensembl_release = hash.metadata.ensembl_release_number;
+    hash.custom = hash.metadata.custom;
     return super.normalize(type, hash);
   }
 }
