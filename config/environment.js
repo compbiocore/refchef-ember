d419cd5fbff8ae3cd948ba33143537dd27905a14
fernandogelin <fernandogelin@gmail.com>
2019-11-22 10:25:52 -0500

    fix: fix environment dev

--- config/environment.js
+++ config/environment.js
@@ -24,6 +24,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.rootURL = '/refchef-ember/';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -33,7 +34,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.rootURL = '/refchef-ember/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
